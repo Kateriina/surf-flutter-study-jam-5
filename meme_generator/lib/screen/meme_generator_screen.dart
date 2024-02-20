84779d3fa6e8f157b0591663c05fb498027e33d9
Kateriina <blondinkova@mail.ru>
2024-02-20 17:52:44 +0300

    Implemented one screen of the demotivator app. Features: change the image by uploading it from the gallery or by url, change the text, share the created image.

--- meme_generator/lib/screen/meme_generator_screen.dart
+++ meme_generator/lib/screen/meme_generator_screen.dart
@@ -1,7 +1,117 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class MemeGeneratorScreen extends StatelessWidget {
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:share_plus/share_plus.dart';
+
+class MemeGeneratorScreen extends StatefulWidget {
   const MemeGeneratorScreen({Key? key}) : super(key: key);
+
+  @override
+  _MemeGeneratorScreenState createState() => _MemeGeneratorScreenState();
+}
+
+class _MemeGeneratorScreenState extends State<MemeGeneratorScreen> {
+  String imageUrl =
+      'https://i.cbc.ca/1.6713656.1679693029!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/this-is-fine.jpg';
+
+  //String memeText = 'Для начала улыбнись';
+  String memeText = 'Здесь мог бы быть ваш мем';
+
+  void _pickImageFromGallery() async {
+    final _picker = ImagePicker();
+    XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        imageUrl = pickedFile.path;
+      });
+    }
+  }
+
+  void _pickImage() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String url = '';
+        return AlertDialog(
+          title: Text('Выбрать картинку'),
+          content: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                    url = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Введите URL',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _pickImageFromGallery();
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.attach_file),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageUrl = url;
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Сохранить'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Метод для обмена полученным мемом
+  void _shareMeme() {
+    Share.share(imageUrl);
+  }
+
+  // Метод для редактирования текста мема
+  void _editText() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Изменить текст'),
+          content: TextField(
+            onChanged: (value) {
+              setState(() {
+                memeText = value;
+              });
+            },
+            decoration: InputDecoration(
+              hintText: 'Введите текст',
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Ок'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,47 +123,86 @@ class MemeGeneratorScreen extends StatelessWidget {
     );
     return Scaffold(
       backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Генератор мемов'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: _editText,
+          ),
+          IconButton(
+            icon: Icon(Icons.image),
+            onPressed: _pickImage,
+          ),
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: _shareMeme,
+          ),
+        ],
+      ),
       body: Center(
-        child: ColoredBox(
-          color: Colors.black,
-          child: DecoratedBox(
-            decoration: decoration,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 50,
-                vertical: 20,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    width: double.infinity,
-                    height: 200,
-                    child: DecoratedBox(
-                      decoration: decoration,
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Image.network(
-                          'https://i.cbc.ca/1.6713656.1679693029!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/this-is-fine.jpg',
-                          fit: BoxFit.cover,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            double imageSize = constraints.maxWidth * 0.8;
+
+            return ColoredBox(
+              color: Colors.black,
+              child: DecoratedBox(
+                decoration: decoration,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 20,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: _pickImage,
+                        child: SizedBox(
+                          width: imageSize,
+                          height: imageSize,
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: imageUrl.startsWith('http')
+                                  ? CachedNetworkImage(
+                                      imageUrl: imageUrl,
+                                      fit: BoxFit.cover,
+                                    )
+                                  : Image.file(
+                                      File(imageUrl),
+                                      fit: BoxFit.cover,
+                                    ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      GestureDetector(
+                        onTap: _editText,
+                        child: Text(
+                          memeText,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Impact',
+                            fontSize: 40,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  const Text(
-                    'Здесь мог бы быть ваш мем',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Impact',
-                      fontSize: 40,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
